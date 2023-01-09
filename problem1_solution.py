d435b764d97a74e71296b55b6d6aadd1aa7adaa6
amirhoseynbaratpoor <74157932+amirhoseynbaratpoor@users.noreply.github.com>
2023-01-09 16:58:05 +0330

    Update problem1_solution.py

--- problem1_solution.py
+++ problem1_solution.py
@@ -1,13 +1,13 @@
-def swap_num(in_text: str, num_max: int):
-    num_max = 10
+def ahbp(in_text: str, num_max: int):
     len_a = len(in_text)
-    c = 1
     while True:
-        if len(in_text)>l_to_reach:
-            in_text = in_text[:l_to_reach]
+        if len(in_text)>num_max:
+            in_text = in_text[:num_max]
             break
         in_text += in_text
     
     return in_text.count("a"), in_text 
     
-num_str, out_text = swap_num("aba", 10)
+num_str, out_text = ahbp("aba", 10)
+print(f"string: {out_text}")
+print(f"The number of characters a: {num_str}")
