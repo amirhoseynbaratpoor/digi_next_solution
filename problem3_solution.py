ec5e20f7cdf45dbb3516752241d6afa7f6218ba9
amirhoseynbaratpoor <74157932+amirhoseynbaratpoor@users.noreply.github.com>
2023-01-09 17:01:10 +0330

    Update problem3_solution.py

--- problem3_solution.py
+++ problem3_solution.py
@@ -1,29 +1,29 @@
 def minSwaps(arr):
-	n = len(arr)
-	arrpos = [*enumerate(arr)]
-	arrpos.sort(key=lambda it: it[1])
-	vis = {k: False for k in range(n)}
-	ans = 0
-	for i in range(n):
+    n = len(arr)
+    arrpos = [*enumerate(arr)]
+    arrpos.sort(key=lambda it: it[1])
+    vis = {k: False for k in range(n)}
+    ans = 0
+    for i in range(n):
 
-		if vis[i] or arrpos[i][0] == i:
-			continue
-			
-		cycle_size = 0
-		j = i
+        if vis[i] or arrpos[i][0] == i:
+            continue
+            
+        cycle_size = 0
+        j = i
 
-		while not vis[j]:
-
-
-			vis[j] = True
+        while not vis[j]:
 
 
-			j = arrpos[j][0]
-			cycle_size += 1
+            vis[j] = True
 
-		if cycle_size > 0:
-			ans += (cycle_size - 1)
 
-	return ans
-	
+            j = arrpos[j][0]
+            cycle_size += 1
+
+        if cycle_size > 0:
+            ans += (cycle_size - 1)
+
+    return ans
+    
 print(minSwaps([7 ,1 ,3, 2 , 4, 5, 6]))
