0da54275a860b6626f7a67e1eeaa132a858935d7
amirhoseynbaratpoor <74157932+amirhoseynbaratpoor@users.noreply.github.com>
2023-01-09 16:38:42 +0330

    updated problem2_solution.py file

--- problem2_solution.py
+++ problem2_solution.py
@@ -1,9 +1,8 @@
+import re
 def no_rep(in_str):
     in_no_space = re.sub(" +", "", in_str)
     out = "".join(re.findall(r"(\w)\1*", in_no_space))
-    num_repl = len(string_in) - len(out)
+    num_repl = len(in_str) - len(out)
     return num_repl
-	
-	
 	
 no_rep("AABAAB")
